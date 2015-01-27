179f4e90216791283de14891db761bd8adf063ce
T-107 <T-107@PC249>
2015-01-26 19:50:04 -0600

    TERMINAMOS

---
+++ src/octavo/java/cap1/AplicacionIMC.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package octavo.java.cap1;
+
+public class AplicacionIMC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //primero creamos un objeto de tipo imc
+        IMC xxx= new IMC();
+        //IMC se le llama instancia, lo que esta a la derecha del = 
+        //izquieda referencia
+        float resultado = 
+        xxx.calcular(1.68f, 86);
+        System.out.println("Tu imc es: " +resultado);
+                
+    }
+    
+}
---
+++ src/octavo/java/cap1/IMC.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package octavo.java.cap1;
+
+/**
+ *
+ * @author T-107
+ */
+public class IMC {
+    private float estatura;
+    private float peso;
+    public void  setpeso(float peso){
+        this.peso=peso;
+    }
+    public void  setestatura (float estatura){
+        this.estatura=estatura;
+    }
+    public float calcular(){
+        float algo=peso / (estatura*estatura);
+        return algo;
+        //return peso/(estatura*estatura);
+    }
+}
